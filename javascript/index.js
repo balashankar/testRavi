4d361bcafd04199448e96568c38d631ac4b4c4f0
balashankar <balashankar@users.noreply.github.com>
2023-10-21 21:32:00 +0530

    Add files via upload

--- javascript/index.js
+++ javascript/index.js
@@ -9,14 +9,18 @@ const port = 3001;
 // Parse JSON request bodies
 app.use(express.json());
 
+
+//Need to run 'npm i path' to install path header. This will integrate and give access file paths.
+const path = require('path');
+
 // Route handlers to be defined below
 app.get("/api/tree", getTree);
 app.post("/api/tree", postTree);
 
 // front-end file to get/post API calls
-app.get("/tree", (req, res) => {
-  res.sendFile("index.html", { root: __dirname + "/" });
-});
+app.get('^/$|/home(.html)?',(req, res)=>{
+  res.sendFile(path.join(__dirname,'views','home.html'));
+})
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
